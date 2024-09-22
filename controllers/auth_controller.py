36f90e503de187ece2b5007dd56386d393675399
perripez <pezzadkins@gmail.com>
2024-09-22 14:37:27 +1000

    Added Validation of the entries, added the update user route and controller

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,10 +1,14 @@
+from datetime import timedelta
+
 from flask import Blueprint, request
-from models.user import User, user_schema
+
+from models.user import User, user_schema, UserSchema
 from init import bcrypt, db
+
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
-from flask_jwt_extended import create_access_token
-from datetime import timedelta
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -12,7 +16,7 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 def register_user():
     try:
         # Get the data from the body of the request
-        body_data = request.get_json()
+        body_data = UserSchema().load(request.get_json())
         # Create an instance of the user model
         user = User(
             name = body_data.get("name"),
@@ -53,3 +57,28 @@ def login_user():
     # Else, return error message
     else:
         return {"error": "Invalid email or password"}, 400
+
+# UPDATE user: /auth/users/user_id
+@auth_bp.route("/users/", methods=["PUT", "PATCH"])
+@jwt_required()
+def update_user():
+    # get the fields from the body of the request
+    body_data = UserSchema().load(request.get_json(), partial=True)
+    password = body_data.get("password")
+    # fetch the user from the db
+    stmt = db.select(User).filter_by(id=get_jwt_identity())
+    user = db.session.scalar(stmt)
+    # if exists:
+    if user:
+        # update the fields as required
+        user.name = body_data.get("name") or user.name
+        if password:
+            user.password = bcrypt.generate_password_hash(password).decode("utf-8")
+        # commit to the db
+        db.session.commit()
+        # return a response
+        return user_schema.dump(user)
+    # else:
+    else:
+        # return an error response
+        return "The user does not exist"
